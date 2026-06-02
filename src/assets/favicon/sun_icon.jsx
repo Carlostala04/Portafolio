3e1ccec8305c7ebc69f2e540c1d9e73ebeea4c2b
Carlostala04 <carlostala.dev@gmail.com>
2026-06-02 17:58:11 -0600

    se agrego el icono del sol

---
+++ src/assets/favicon/sun_icon.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+
+export function SunLinear(props) {
+  return (
+    <svg
+      {...props}
+      xmlns="http://www.w3.org/2000/svg"
+      width="1em"
+      height="1em"
+      viewBox="0 0 24 24"
+    >
+      <title xmlns="">sun-linear</title>
+      <g fill="none" stroke="currentColor" stroke-width="1.5">
+        <circle cx="12" cy="12" r="6" />
+        <path
+          stroke-linecap="round"
+          d="M12 2v1m0 18v1m10-10h-1M3 12H2m17.07-7.07l-.392.393M5.322 18.678l-.393.393m14.141-.001l-.392-.393M5.322 5.322l-.393-.393"
+        />
+      </g>
+    </svg>
+  );
+}
+
+export default SunLinear;
