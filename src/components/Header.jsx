713cd171f7e2fd43267ec7cfe5a8cd3734139a58
Carlostala04 <carlostala.dev@gmail.com>
2026-06-11 14:09:04 -0600

    se mejoro la estructurua del header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,27 +1,36 @@
 import React from "react";
 import Sun from "../assets/favicon/sun_icon";
 import Moon from "../assets/favicon/moon_icon";
-export default function Header({toggleTheme, darkMode}) {
+import "../styles/header.css";
+export default function Header({ toggleTheme, darkMode }) {
   return (
     <header>
+      <span className="square"></span>
       <h4>Carlos Talavera</h4>
-      <ul>
-        <li>
-          <a href="">Sobre mi</a>
-        </li>
-        <li>
-          <a href="">Stack</a>
-        </li>
-        <li>
-          <a href="">Proyectos</a>
-        </li>
-        <li>
-          <a href="">Experiencia</a>
-        </li>
-        <li>
-          <a href="">Contacto</a>
-        </li>
-      </ul>
+      <nav className="nav-bar">
+        <ul className="item-list">
+          <li className="item">
+            <a className="link" href="">
+              Sobre mi
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="">
+              Proyectos
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="">
+              Stacks
+            </a>
+          </li>
+          <li className="item">
+            <a className="link" href="">
+              Contactame
+            </a>
+          </li>
+        </ul>
+      </nav>
       <div className="language-buttons">
         <button>EN</button>
         <button>ES</button>
