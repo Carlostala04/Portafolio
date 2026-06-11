75564669e5e3f8d28e78f68136c33f79c433777c
Carlostala04 <carlostala.dev@gmail.com>
2026-06-11 14:08:36 -0600

    se cambiaron las props de las cards

--- src/components/CardProyects.jsx
+++ src/components/CardProyects.jsx
@@ -1,25 +1,44 @@
 import React from "react";
 import LinkIcon from "../assets/favicon/link_icon";
+import GitHubIcon from "../assets/favicon/github_icon";
+import "../styles/cardProyect.css";
+import "../styles/svg.css";
+
 export default function CardProyects({
-  url,
-  urlGitHub,
-  image,
-  title,
-  description,
+  imagen,
+  titulo,
+  descripcion,
+  tecnologias,
+  repositorio,
+  preview,
 }) {
   return (
     <article className="card-proyect">
       <div className="card-proyect-image">
-        <img src={image} alt="Imagen de proyecto" />
+        <img src={imagen} alt={titulo} />
       </div>
+
       <div className="card-proyect-description">
-        <h4>{title}</h4>
-        <p>{description}</p>
+        <h4>{titulo}</h4>
+        <p>{descripcion}</p>
       </div>
+
+      <div className="card-proyect-technologies">
+        {tecnologias.map(({ Icono }, index) => (
+          <div className="tech" key={index}>
+            <Icono />
+          </div>
+        ))}
+      </div>
+
       <div className="card-proyect-actions">
-        <a href={urlGitHub}>Github</a>
-        <a href={url}>
+        <a href={repositorio} target="_blank" rel="noopener noreferrer">
+          <GitHubIcon />
+          Repositorio
+        </a>
+        <a href={preview} target="_blank" rel="noopener noreferrer">
           <LinkIcon />
+          Ver preview
         </a>
       </div>
     </article>
