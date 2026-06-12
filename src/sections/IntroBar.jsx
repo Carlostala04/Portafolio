a148e5417068d8b2bfccc3636e660831520a697a
Carlostala04 <carlostala.dev@gmail.com>
2026-06-12 17:56:34 -0600

    se creo la seccion de introduccion

---
+++ src/sections/IntroBar.jsx
@@ -0,0 +1,18 @@
+export default function IntroBar() {
+  return (
+    <section className="intro-bar">
+      <div className="intro-bar-item">
+        <span className="comment-data">// Carlos Talavera</span>
+        <h5 className="data">Frontend/React</h5>
+      </div>
+      <div className="intro-bar-item">
+        <span className="comment-data">// Status</span>
+        <h5 className="data">Disponible · Q3 2026</h5>
+      </div>
+      <div className="intro-bar-item">
+        <span className="comment-data">// Location</span>
+        <h5 className="data flag">Nicaragua</h5>
+      </div>
+    </section>
+  );
+}
