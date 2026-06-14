34a674c9a9f015b714886d768e748f0607d42c04
Carlostala04 <carlostala.dev@gmail.com>
2026-06-13 22:20:54 -0600

    se cambio el temaño de los titulos de los data

--- src/sections/IntroBar.jsx
+++ src/sections/IntroBar.jsx
@@ -1,17 +1,18 @@
+import "../styles/introBar.css";
 export default function IntroBar() {
   return (
     <section className="intro-bar">
       <div className="intro-bar-item">
         <span className="comment-data">// Carlos Talavera</span>
-        <h5 className="data">Frontend/React</h5>
+        <h4 className="data">Frontend/React</h4>
       </div>
       <div className="intro-bar-item">
         <span className="comment-data">// Status</span>
-        <h5 className="data">Disponible · Q3 2026</h5>
+        <h4 className="data">Disponible · Q3 2026</h4>
       </div>
       <div className="intro-bar-item">
         <span className="comment-data">// Location</span>
-        <h5 className="data flag">Nicaragua</h5>
+        <h4 className="data data-flag">Costa Rica</h4>
       </div>
     </section>
   );
