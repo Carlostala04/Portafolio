c84a4e3703803f485ed6348d25e42e78d2c824d9
Carlostala04 <carlostala.dev@gmail.com>
2026-06-14 15:25:50 -0600

    se modifico toda la estructura del componente button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,5 +1,23 @@
 import React from "react";
 import "../styles/button.css";
-export default function Button({ title, Icon:icon }) {
-  return <a className="button-default">{title} {icon}</a>;
+
+export default function Button({ title, Icon: icon, href, target }) {
+  if (href) {
+    return (
+      <a
+        className="button-default"
+        href={href}
+        target={target}
+        rel={target === "_blank" ? "noopener noreferrer" : undefined}
+      >
+        {title} {icon}
+      </a>
+    );
+  }
+
+  return (
+    <button className="button-default">
+      {title} {icon}
+    </button>
+  );
 }
